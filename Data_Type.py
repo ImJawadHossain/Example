5eb89aab432d857b95d4e69a68b50382f8e44d29
Md Jawad Hossain <jawadsgs3@gmail.com>
2020-12-01 07:54:08 +0600

    Create Data_Type.py
    If you no remember what is list, tuple, set etc. about data type you see the example and get it easily.

---
+++ Data_Type.py
@@ -0,0 +1,41 @@
+# Data type in python
+
+# 1. Int data type
+integer = 1 + 2 + 3 + 4                       # Ist int type data
+print(1, type(integer), integer)
+
+# 2. Float type data
+floating = 1.1 + 5.4 + 3.8 + 19.4             # Its float type data
+print(2, type(floating), floating)
+
+# 3. Complex data type
+com = 5 + 4j + 30 + 87j                       # Its complex type data
+print(3, type(com), com)
+
+# 4. Bool type data
+boolean = True                                # Its bool type data
+print(4, type(boolean), boolean)
+
+# 5. Str data type
+string = 'Md Jawad Hossain'                   # Its str type data
+print(5, type(string), string)
+
+# 6. List type data
+lis = [10, 12, 1.5, -56]                      # Its list type data
+print(6, type(lis), lis)
+
+# 7. Tuple data type
+tup = (10, 27.6, -21, 'Jawad')                # Its tuple type data
+print(7, type(tup), tup)
+
+# 8. Range data type
+rg = range(2, 10, 2)                          # Its range type data
+print(8, type(rg), rg)
+
+# 10. Set data type
+st = {12, 34, 'Jawad', 6.5, -9, 12, 'Jawad'}  # Its set type data   # not print duplicate item
+print(10, type(st), st)
+
+# 11. Dictionary data type
+dt = {'Jawad': 101, 'Hossain': 102}           # Its dictionary type data
+print(11, type(dt), dt['Jawad'])
